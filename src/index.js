b56620a83ac2dd95646b0a69f485cc0e75ef6f35
Leonardo Gatica <lgatica@protonmail.com>
2016-01-08 20:14:59 +0000

    Add number, message and description

--- src/index.js
+++ src/index.js
@@ -9,14 +9,19 @@ import pkg from '../package.json';
 
 const getBalance = (number) => {
   bip(number).then((data) => {
-    console.log(`Saldo: ${data.balance}`);
-    console.log(`Fecha de saldo: ${moment(data.date).format('DD/MM/YYYY HH:mm')}`);
+    console.log(`Número: ${data.number}`);
+    console.log(`Saldo: $${data.balance}`);
+    if (data.date) {
+      console.log(`Fecha de saldo: ${moment(data.date).format('DD/MM/YYYY HH:mm')}`);
+    }
+    console.log(`Mensaje: ${data.message}`);
   }).catch((err) => console.log(err.message));
 };
 
 program
   .version(pkg.version)
   .usage('<numero tarjeta bip>')
+  .description('Obtener saldo de tarjeta bip')
   .action(getBalance)
   .parse(process.argv);
 
